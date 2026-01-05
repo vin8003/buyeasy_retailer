02cb39b141639ffc964599331d9b7dc815b3384a
vin8003 <vin8003@gmail.com>
2026-01-05 21:15:44 +0530

    revert server baseurl

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -14,7 +14,7 @@ class ApiService {
   String? _accessToken;
   String? _refreshToken;
 
-  String _baseUrl = 'http://127.0.0.1:8000/api/';
+  String _baseUrl = 'https://api.ordereasy.win/api/';
 
   // Navigation key to allow navigating from outside the widget tree
   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
@@ -129,7 +129,7 @@ class ApiService {
   }
 
   Future<void> _initBaseUrl() async {
-    _baseUrl = 'http://127.0.0.1:8000/api/';
+    _baseUrl = 'https://api.ordereasy.win/api/';
     _dio.options.baseUrl = _baseUrl;
   }
 
@@ -221,7 +221,7 @@ class ApiService {
   }
 
   Future<void> checkAuthToken() async {
-    _baseUrl = 'http://127.0.0.1:8000/api/';
+    _baseUrl = 'https://api.ordereasy.win/api/';
     _dio.options.baseUrl = _baseUrl;
     SharedPreferences prefs = await SharedPreferences.getInstance();
     _accessToken = prefs.getString('access_token');
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String _serverUrl = 'http://127.0.0.1:8000';
+  static const String _serverUrl = 'https://api.ordereasy.win';
 
   static String get serverUrl => _serverUrl;
   static String get baseUrl => '$serverUrl/api';
